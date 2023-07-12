484567e85c56a86fde3d65eff80bf1d12b83d4f2
isahua16 <isaelhuard@gmail.com>
2023-07-11 21:24:18 -0600

    added signup form

--- src/components/RegisterForm.vue
+++ src/components/RegisterForm.vue
@@ -1,7 +1,28 @@
 <template>
-  <div></div>
+  <v-main>
+    <v-container>
+      <v-text-field
+        :elevation="hover ? 24 : 6"
+        label="email"
+        type="text"
+        ref="input_username"
+      ></v-text-field>
+      <v-text-field
+        :elevation="hover ? 24 : 6"
+        label="username"
+        type="text"
+        ref="input_username"
+      ></v-text-field>
+      <v-text-field
+        :elevation="hover ? 24 : 6"
+        label="password"
+        type="password"
+        ref="input_password"
+      ></v-text-field>
+      <v-btn depressed color="primary"> Submit </v-btn>
+    </v-container>
+  </v-main>
 </template>
-
 <script>
 export default {};
 </script>
